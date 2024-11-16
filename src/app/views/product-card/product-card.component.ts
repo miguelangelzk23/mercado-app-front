c76758b7ca70f15e10c89f765438cc4be65265db
MIguel <miguelangelzk0405@gmail.com>
2024-11-15 19:13:13 -0500

    add componente de cantidad

--- src/app/views/product-card/product-card.component.ts
+++ src/app/views/product-card/product-card.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
+  isQuantitySelectorVisible = false; // Controla si el selector de cantidad está visible
+  quantity = 1; // Cantidad inicial
 
+  // Muestra el selector de cantidad
+  showQuantitySelector() {
+    this.isQuantitySelectorVisible = true;
+  }
+
+  // Incrementa la cantidad
+  increaseQuantity() {
+    this.quantity++;
+  }
+
+  // Decrementa la cantidad (asegurando que no sea menor a 1)
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
 }
